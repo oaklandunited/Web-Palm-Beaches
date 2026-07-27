70c03d84bf2bc3a22184f4fb3cf4e1922394709b
Oakland United <gregg@oaklandunited.com>
2026-07-27 12:59:11 -0400

    Qwen

---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'DbfY1H3Pu6URE1RQLLnKLJUIBhyw6PXF3pEvAstBHm2V1xJ1tsJhng5m6ImhXiEa');
+define('SECURE_AUTH_KEY',  'XDxy8ABiiT1UC40b2cPtFD5VPNoQrrnAD632Ma5G4qzRsDjsut7z5mzCHyB8L17R');
+define('LOGGED_IN_KEY',    '6f0eKBr3QdESroHDLuUKMxFeJphWYuNKzh44Dmf6JnsFq84K5NUAgjbG1JmxdzqR');
+define('NONCE_KEY',        'yYbAFQDC3Sr9cYpWMWGf8ouTMNISW2x88HrqYU6WMNfGXtIm0dHQaK5PUvdE0wof');
+define('AUTH_SALT',        '2Y4CLzK3CUy5EdyMMN98yLPTNUBze0H1KrgVY5heAbzr30Xs7NeL5s9z1jjNUrTX');
+define('SECURE_AUTH_SALT', 'vnMq7SKPPXv47GSRhCCUyGDGNygL9Sh3LycroM4BiDxBNfivJ5up7I1cqPaUnmxD');
+define('LOGGED_IN_SALT',   'xfiJi7MRGRMnXWIdii1xI0CiurXKNo0zjT4KAunNV2qdne3HUA10EgMP41bB4r0c');
+define('NONCE_SALT',       'L1YPLTQGPKLow4FEY1GoWJCCPmXBHSEEQU4W0fUcw62tyJug96yMjnmA60ExPbHL');
